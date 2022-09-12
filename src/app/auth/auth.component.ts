c0c2d90dcc2c997d781ac6bc551b04f8c05d9f97
gucal <gucalaliriza@outlook.com>
2022-09-12 13:19:11 +0300

    auth page add

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor() { }
+  authForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    this.authForm.reset();
   }
-
 }
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { AuthComponent } from '../auth/auth.component';
 import { LayoutComponent } from './layout.component';
 
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [AuthComponent, LayoutComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    InputTextModule,
+    ButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
