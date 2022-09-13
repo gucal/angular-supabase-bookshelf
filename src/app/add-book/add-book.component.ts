abb004ea54094ecdd0e4daf032e336e39c2888b8
gucal <gucalaliriza@outlook.com>
2022-09-13 11:41:22 +0300

    book create form - dropdown

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Category {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
-  styleUrls: ['./add-book.component.css']
+  styleUrls: ['./add-book.component.css'],
 })
 export class AddBookComponent implements OnInit {
+  categories: Category[] = [
+    {
+      id: 1,
+      name: 'Gerilim',
+    },
+    {
+      id: 2,
+      name: 'Korku',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  selectCategoryID?: Category;
 }
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -7,10 +7,24 @@ import { MenubarModule } from 'primeng/menubar';
 import { TableModule } from 'primeng/table';
 import { TableComponent } from '../table/table.component';
 import { AddBookComponent } from '../add-book/add-book.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [LayoutComponent, TableComponent, AddBookComponent],
-  imports: [CommonModule, MenubarModule, TableModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MenubarModule,
+    TableModule,
+    InputTextModule,
+    ButtonModule,
+    DropdownModule,
+  ],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
