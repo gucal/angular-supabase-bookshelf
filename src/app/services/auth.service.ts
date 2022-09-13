280f27be32e826887abdeac5539d3ed20632561b
gucal <gucalaliriza@outlook.com>
2022-09-13 14:45:39 +0300

    add auth redirect

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { SupabaseService } from './supabase.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private supabase: SupabaseService) {}
+  constructor(private supabase: SupabaseService, private router: Router) {}
 
   async login(email: string, password: string) {
     const isLogin: any = await this.supabase.authControl(email, password);
 
     if (isLogin) {
-      alert('basarili');
+      this.router.navigate(['/list']);
     } else alert('basarisiz');
   }
 }
