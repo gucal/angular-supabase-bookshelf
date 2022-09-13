83869044b271d50d8912892299c5e55ae6e39320
gucal <gucalaliriza@outlook.com>
2022-09-13 10:28:07 +0300

    primeng table component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 
 import { MenubarModule } from 'primeng/menubar';
+import { TableModule } from 'primeng/table';
 import { TableComponent } from '../table/table.component';
 
 @NgModule({
   declarations: [LayoutComponent, TableComponent],
-  imports: [CommonModule, MenubarModule],
+  imports: [CommonModule, MenubarModule, TableModule],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  products: any = [
+    {
+      name: 'Ali',
+      code: 'Riza',
+      category: 'Gucal',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
