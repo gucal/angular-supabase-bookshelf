639431ffda242b605e67dece7ed5ac927bc62732
gucal <gucalaliriza@outlook.com>
2022-09-15 10:32:49 +0300

    production env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   supabaseUrl: 'https://lxluolrwancwiqjeerto.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4bHVvbHJ3YW5jd2lxamVlcnRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjI5NjQwMTcsImV4cCI6MTk3ODU0MDAxN30.8D7RILhzqlxhqt5ILWf0jAA2AwsJzPVTZ63b84upBao',
 }
